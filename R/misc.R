fe6f3a4ba6e22116fbe5ce7bccde531cb70c2e98
Alex Brownrigg <BrownriggA01@xleedspct.nhs.uk>
2022-08-12 14:12:07 +0100

    feat:create func

---
+++ R/misc.R
@@ -0,0 +1,24 @@
+library(devtools)
+getwd()
+setwd(dir = "C:/Users/BROWNRIGGA01/Documents/R/regexcite")
+use_git()
+
+(x <- "alfa,bravo,charlie,delta")
+#> [1] "alfa,bravo,charlie,delta"
+strsplit(x, split = ",")
+#> [[1]]
+#> [1] "alfa"    "bravo"   "charlie" "delta"
+
+unlist(strsplit(x, split = ","))
+#> [1] "alfa"    "bravo"   "charlie" "delta"
+
+strsplit(x, split = ",")[[1]]
+#> [1] "alfa"    "bravo"   "charlie" "delta"
+
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+
+
+load_all(path = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/")
+

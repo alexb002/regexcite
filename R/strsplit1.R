12c6c6882d29c35a3c28ba1e90b43504c8239fe6
alexb002 <a.brownrigg@nhs.net>
2022-08-15 11:46:12 +0100

    feat: pkg testing git log q
    :q
    ls

--- R/strsplit1.R
+++
@@ -1,4 +0,0 @@
-strsplit1 <- function(x, split) {
-  # cat("checking")
-  strsplit(x, split = split)[[1]]
-}
--- misc.R
+++ misc.R
@@ -1,6 +1,9 @@
+# https://r-pkgs.org/Whole-game.html
+
 library(devtools)
 getwd()
 setwd(dir = "C:/Users/BROWNRIGGA01/Documents/R/regexcite")
+
 use_git()
 
 (x <- "alfa,bravo,charlie,delta")
@@ -19,16 +22,62 @@ strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
 
-setwd(dir = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/")
 use_r("strsplit1")
 load_all(path = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/R")
 
-usethis::git_sitrep()
-usethis::use_git_config(scope = "user")
-usethis::git_vaccinate()
+# usethis::git_sitrep()
+# usethis::use_git_config(scope = "user")
+# usethis::git_vaccinate()
 
-remove.packages("devtools")
-install.packages("devtools")
-packageVersion(pkg = "devtools")
+(x <- "alfa,bravo,charlie,delta")
+#> [1] "alfa,bravo,charlie,delta"
+strsplit1(x, split = ",")
+#> [1] "alfa"    "bravo"   "charlie" "delta"
 
-load_all()
+exists("strsplit1", where = globalenv(), inherits = FALSE)
+#> [1] FALSE
+
+check()
+
+# pkgbuild::check_build_tools(debug = TRUE)
+
+use_mit_license()
+
+document()
+?strsplit1
+
+check()
+
+install()
+
+library(regexcite)
+x <- "alfa,bravo,charlie,delta"
+strsplit1(x, split = ",")
+#> [1] "alfa"    "bravo"   "charlie" "delta"
+
+use_testthat()
+use_test("strsplit1")
+#> ✔ Writing 'tests/testthat/test-strsplit1.R'
+#> • Edit 'tests/testthat/test-strsplit1.R'
+
+library(testthat)
+load_all(path = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/R")
+load_all(path = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/tests/")
+
+test()
+use_package("stringr")
+#> ✔ Adding 'stringr' to Imports field in DESCRIPTION
+#> • Refer to functions with `stringr::fun()`
+
+rename_files("strsplit1", "str_split_one")
+#> ✔ Moving 'R/strsplit1.R' to 'R/str_split_one.R'
+#> ✔ Moving 'tests/testthat/test-strsplit1.R' to 'tests/testthat/test-str_split_one.R'
+
+use_testthat()
+use_test("str_split_one")
+
+document()
+
+load_all(path = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/R")
+load_all(path = "C:/Users/BROWNRIGGA01/Documents/R/regexcite/tests/")
+
